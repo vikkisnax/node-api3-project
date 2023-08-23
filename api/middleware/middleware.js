ab392a750d9455aa564ae1f80eabeaae94a168ca
Victoria Blancas <vikki6688@gmail.com>
2023-08-23 17:09:50 -0400

    wip

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,24 +1,33 @@
 function logger(req, res, next) {
-  console.log(
-    `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get('Origin')}`
-  );
-  next();
+  // console.log('logger:', logger);
+  const timestamp = new Date().toLocaleString()
+  //method - get put, etc. is inside req.method
+  const method = req.method
+  // url - can find it in many places
+  const url = req.originalUrl
+  console.log(`[${timestamp} ${method} to ${url}]`)
+  next()
 }
 
 function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+  console.log('validateUserId:', validateUserId)
+  next()
 }
 
 function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+  console.log('validateUser:', validateUser)
+  next()
 }
 
 function validatePost(req, res, next) {
-  // DO YOUR MAGIC
+  console.log('validatePost:', validatePost)
+  next()
 }
 
 // do not forget to expose these functions to other modules
 module.exports = {
   logger,
-  
+  validateUserId,
+  validateUser,
+  validatePost
 }
