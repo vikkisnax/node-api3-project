405075109b4cad813aaed0a722c7a3cb2fd0e534
Victoria Blancas <vikki6688@gmail.com>
2023-08-23 12:01:51 -0400

    wip

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,5 +1,8 @@
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get('Origin')}`
+  );
+  next();
 }
 
 function validateUserId(req, res, next) {
@@ -15,3 +18,7 @@ function validatePost(req, res, next) {
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  
+}
--- api/server.js
+++ api/server.js
@@ -1,13 +1,25 @@
-const express = require('express');
+// importing a CommonJS module
+const express = require('express'); 
+
+// import/require users-router.js
+
 
 const server = express();
 
-// remember express by default cannot parse JSON in request bodies
+// remember express by default cannot parse JSON in request bodies, so use this code 
+server.use(express.json());
 
 // global middlewares and the user's router need to be connected here
 
+
+//dummy test endpoint
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
+});
+
+server.use('*', (req, res) => {
+  // catch all 404 errors middleware
+  res.status(404).json({ message: `${req.method} ${req.baseUrl} not found!` });
 });
 
 module.exports = server;
